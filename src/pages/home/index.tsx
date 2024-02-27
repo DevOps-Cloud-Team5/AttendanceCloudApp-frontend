95a31da7162c686f7c80f9cff2a70e5226e9ad65
S-vz <s.vrzina@student.vu.nl>
2024-02-27 11:47:47 +0100

    auto deploy test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,7 @@ const HomePage: FC = () => {
     return (
         <Rootpage>
             <h1>This is the Home Page!</h1>
+            <h2>With an updated header!</h2>
         </Rootpage>
     );
 };
