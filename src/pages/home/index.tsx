4356dc3706527e18150c164104a990e5ebf68703
Priyanka <priyankaatwani@live.com>
2024-02-29 13:43:35 +0100

    chnaged login and home page a bit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,34 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import Rootpage from "../root";
+import { Box, Button, Typography } from "@mui/material";
+import backgroundImage from "../../assets/logo.png";
 
 const HomePage: FC = () => {
-    return (
-        <Rootpage>
-            <h1>This is the Home Page!</h1>
-            <h2>With an updated header!</h2>
-            <h2>An a second header!</h2>
-        </Rootpage>
-    );
+  return (
+    <Rootpage>
+      <Box
+        sx={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            color: "black",
+            padding: 8,
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            minHeight: "85vh",
+        }}
+      >
+        <Typography variant="h3" component="h1" gutterBottom justifyContent={'center'}>
+          Welcome to Attendunce
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom >
+          The ultimate attendance tracking solution
+        </Typography>
+      </Box>
+    </Rootpage>
+  );
 };
 
 export default HomePage;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,19 @@
 import React, { FC, useState } from "react";
 import RootPage from "../root";
-import "./login.css"; // Import CSS file for styling
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import "./login.css"; // Import CSS file for additional styling
+import { Box, FormControlLabel } from "@mui/material";
 
-const LoginPage: FC = () => {
+function SignIn() {
     const [rememberMe, setRememberMe] = useState(false);
 
     const handleRememberMeChange = () => {
@@ -14,29 +25,110 @@ const LoginPage: FC = () => {
         // put registration link?
     };
 
-    const handleLoginFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log("Form submitted");
-        // send api request?
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
     };
 
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#3C80D0', 
+            },
+            text: {
+                primary: '#FAFAFA',
+                secondary: 'rgba(255, 255, 255, 0.7)', // Change text color to white
+            },
+            background: {
+                default: '#fff', // Change background color to black
+            },
+            secondary: {
+                main: '#3C80D0',
+              },
+            
+                mode:'light',
+            
+        },
+    })
+    
+
     return (
-        <RootPage>
-            <div className="login-container">
-                <h1 className="login-title">Account Login</h1>
-                <form className="login-form" onSubmit={handleLoginFormSubmit}>
-                    <input type="text" placeholder="Username" required /><br />
-                    <input type="password" placeholder="Password" required /><br />
-                    <label className="remember">
-                        <input type="checkbox" checked={rememberMe} onChange={handleRememberMeChange} />
-                        Remember Me
-                    </label><br />
-                    <a className="forget" href="#" onClick={handleForgotPassword}>Forgot Password?</a><br />
-                    <button type="submit">Login</button>
-                </form>
-            </div>
+    <RootPage>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} color={"primary"}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            color="primary"
+             
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" checked={rememberMe} onChange={handleRememberMeChange} color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
         </RootPage>
     );
-};
+}
 
-export default LoginPage;
+export default SignIn;
